a879f6883eaa7ed5f212df133dde025ad28e6009
bisoukeiko <87bisou@gmail.com>
2024-11-14 12:14:41 +0100

    task02: first commit

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,47 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (Array.isArray(students)) {
+      if (students.every((item) => typeof item === 'string')) {
+        this._students = students;
+      } else {
+        throw new TypeError('students must be a array of Strings');
+      }
+    } else {
+      throw new TypeError('students must be a array of Strings');
+    }
+  }
+}
