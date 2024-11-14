f36bcab731e788db978eddeda3cb1f464ac234b5
bisoukeiko <87bisou@gmail.com>
2024-11-14 23:59:43 +0100

    task05: first commit

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+    this._sqft = sqft;
+  }
+
+  get sqfth() {
+    return this._sqfth;
+  }
+
+  set sqfth(sqfth) {
+    if (typeof sqfth === 'number') {
+      this._sqfth = sqfth;
+    } else {
+      throw new TypeError('sqfth must be a number');
+    }
+  }
+}
