1107f7223193648f08178175c7148f0b26a9e63d
bisoukeiko <87bisou@gmail.com>
2024-11-15 05:43:16 +0100

    task05: fix code

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,21 +1,20 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
+    this._sqft = sqft;
+
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft nust be a number');
     }
     this._sqft = sqft;
-  }
-
-  get sqfth() {
-    return this._sqfth;
-  }
-
-  set sqfth(sqfth) {
-    if (typeof sqfth !== 'number') {
-      throw new TypeError('sqfth must be a number');
-    }
-    this._sqfth = sqfth;
   }
 }
