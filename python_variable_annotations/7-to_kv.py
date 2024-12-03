a724f16d5f9e9be185dd14c2b5d12cad8bcb664a
bisoukeiko <87bisou@gmail.com>
2024-12-03 16:29:49 +0100

    fix code: import Union, Tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ task07: Complex types - string and int/float to tuple """
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
         arguments:
             k (str):
