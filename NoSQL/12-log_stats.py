d4269b579874cfcccfd200b144cd930710b92fa8
bisoukeiko <87bisou@gmail.com>
2024-12-12 11:38:08 +0100

    first commit

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" task12: Log stats
+    provides some stats about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    logs_collection = client.logs.nginx
+
+    cnt_logs = logs_collection.count_documents({})
+    print("{} logs".format(cnt_logs))
+
+    print("Methods:")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        cnt_method = logs_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, cnt_method))
+
+    cnt_status = logs_collection.count_documents({"path": "/status"})
+    print("{} status check".format(cnt_status))
