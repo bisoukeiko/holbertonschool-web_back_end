781ef35702dadc1d0d095479d8a09ae73705a2ce
bisoukeiko <87bisou@gmail.com>
2025-01-21 17:15:13 +0100

    first commit

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const PORT = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  await countStudents(process.argv[2]).then((studentslist) => {
+    res.send(200, `This is the list of our students\n${studentslist.join('\n')}`);
+  }).catch((err) => {
+    res.send(500, err.message);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('server running');
+});
+
+module.exports = app;
