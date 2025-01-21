d5ed4b9e3ff42876a7ba31367f6a5e5797dee061
bisoukeiko <87bisou@gmail.com>
2025-01-21 14:45:13 +0100

    first commit

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+const countStudents = require('./3-read_file_async');
+
+const filename = process.argv[2];
+
+const port = 1245;
+
+const app = http.createServer(async (request, response) => {
+  response.writeHead(200, {
+    'Content-Type': 'text/html',
+  });
+
+  if (request.url === '/') {
+    response.write('Hello Holberton School!');
+    response.end();
+  }
+
+  if (request.url === '/students') {
+    response.write('This is the list of our students\n');
+
+    try {
+      const students = await countStudents(filename);
+      response.end(`${students.join('\n')}`);
+    } catch (error) {
+      response.end(error.message);
+    }
+  }
+});
+
+app.listen(port);
+
+module.exports = app;
