c5f8ed7304849d25d7fcbf9f0908984ff9dec2c7
bisoukeiko <87bisou@gmail.com>
2025-01-21 17:30:17 +0100

    fix code

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -8,14 +8,16 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  await countStudents(process.argv[2]).then((studentslist) => {
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2]).then((studentslist) => {
     res.send(200, `This is the list of our students\n${studentslist.join('\n')}`);
   }).catch((err) => {
     res.send(500, err.message);
   });
 });
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log('server running');
+});
 
 module.exports = app;
